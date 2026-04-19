ce85613cda3119e687edfa7f464f2abd5270eb43
[ali edris] <[almusdarthafa999@gmail.com]>
2026-04-19 08:03:19 -0700

    updated-and-fix/misMerge-issue

--- mobile/context/authContext.js
+++ mobile/context/authContext.js
@@ -1,6 +1,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { registerForPushNotificationsAsync } from "../utils/registerForPushNotificationsAsync";
+import Instance from "@/lib/axios";
+import Toast from "react-native-toast-message";
 
 const AuthContext = createContext();
 
@@ -15,7 +17,29 @@ export const AuthProvider = ({ children }) => {
         setError(error);
       });
   }, []);
+  const updateExpoPushToken = async () => {
+    try {
+      const res = await Instance.put("/v1/user/update-expo-push-token", {
+        expoPushToken,
+      });
+      if (res.success) {
+        Toast.show({
+          type: "success",
+          text1: "Notification permission granted",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      Toast.show({
+        type: "error",
+        text1: "Failed to get notification permission",
+      });
+    }
+  };
 
+  useEffect(() => {
+    updateExpoPushToken();
+  }, [expoPushToken]);
   return (
     <AuthContext.Provider value={{ expoPushToken }}>
       {children}
